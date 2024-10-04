b7d4e4eb9a65edd047d7df7a013b76a5fe20a536
ext-h-asano <asano@ext.cx>
2024-10-04 14:50:31 +0900

    test

--- index_offer.js
+++ index_offer.js
@@ -77,7 +77,7 @@ function startServerConnection(localId, remoteId) {
 		sc.close();
 	}
 	// サーバー接続の開始
-	sc = new WebSocket('wss://' + '35.78.202.49' + ':' + sslPort + '/');
+	sc = new WebSocket('wss://' + '35.77.212.208' + ':' + sslPort + '/');
 	sc.onmessage = gotMessageFromServer;
 	sc.onopen = function(event) {
 		// サーバーに接続情報を通知
