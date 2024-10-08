793eb86a9a457d1d710beb7a06fdfb6d2a21570d
ext-h-asano <asano@ext.cx>
2024-10-08 10:54:41 +0900

    change IP

--- index_offer.js
+++ index_offer.js
@@ -86,7 +86,7 @@ function startServerConnection(localId, remoteId) {
 		sc.close();
 	}
 	// サーバー接続の開始
-	sc = new WebSocket('wss://' + '35.77.212.208' + ':' + sslPort + '/');
+	sc = new WebSocket('wss://' + '52.68.194.164' + ':' + sslPort + '/');
 	sc.onmessage = gotMessageFromServer;
 	sc.onopen = function(event) {
 		// サーバーに接続情報を通知
